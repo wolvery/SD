d225c3339bf02d68e46907fb8d55ec7b68ef12c1
nandagpx <nanda_gpx@hotmail.com>
2014-12-30 18:13:30 -0200

    Códigos

---
+++ src/jogo/Pecas.java
@@ -0,0 +1,16 @@
+package jogo;
+
+import jogo.Casa;
+
+
+public class Pecas {
+	
+	public static final String LOBO_STRING = "L" ; //lobo
+	
+	public static final String VAZIO_STRING = "X" ; //lobo
+	
+	public static final String PASTOR_STRING = "P" ; //lobo
+	
+	public static final String OVELHA_STRING = "O" ; //lobo
+
+}
--- src/lobo.java
+++ src/lobo.java
@@ -1,4 +1,25 @@
+import jogo.Casa;
+import jogo.Pecas;
 
 public class lobo {
+    
+    public void comerOvelha(Casa atual, Casa ovelha, Jogador numeroOvelhas) {
+    	moverCasa(atual, ovelha);
+    	numeroOvelhas--;
+    }
+    
+    public void moverCasa(Casa atual, Casa proxima) {
+            Pecas pecaLobo = atual.getPeca();
+            atual.setPeca(null);
+            
+            proxima.setPeca(pecaLobo);     
+    }
+    
+    void quebrarCerca(Casa atual, Casa cerca, Jogador forcaCerca) {
+    	if(forcaCerca == 0)
+    		moverCasa(atual, cerca);
+    	else
+    		forcaCerca--;
+    }
 
 }
