59355e89723e5f93520ff79982ac0c4f10bd5244
wolvery <guilhg2@gmail.com>
2015-01-01 14:26:52 -0200

    alteracoes

--- src/jogador/teste.java
+++ src/jogador/teste.java
@@ -1,6 +1,23 @@
 package jogador;
 
 public class teste {
-	Personagem[] novo = new Personagem[10];
+	/**
+	 *  Definimos a quantidade de casas que o jogo possuira'
+	 *  3x3 = 9
+	 *  4x4 = 16
+	 *  5x5 = 25 
+	 */	
+	private static int quantidadeCasas = 9;
+	/**
+	 * Personagens iniciais.
+	 * Lobo, Pastor.
+	 * Resto e' figurante, com estado de vazio.
+	 * 
+	 */
+	Personagem[] jogador = new Personagem[3];
 	
+	public void carregarCasas(int quantidadeCasas, Personagem[] jogador){
+		
+		
+	}
 }
